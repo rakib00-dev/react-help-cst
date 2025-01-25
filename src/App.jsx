254bb4b1dcab24d29a07ce7f473827e234dff391
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-25 12:49:41 +0600

    updated the html and app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 function App() {
-  return (
-    <>
-      <p className="bg-red-500">this is a p tag</p>
-      <h1 class="text-3xl font-bold ">Hello world!</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
