19dc74b42bcf20f61e08fcee83c280a3c912c1d7
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-25 22:45:52 +0600

    fixed the responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
 const Header = () => {
   return (
     <section className="bg-gray-100 w-full text-gray-800 overflow-hidden">
-      <div className="container paddings innerWidth flexCenter flex flex-col-reverse justify-center mx-auto px-20 sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-        <div className="flex flex-col justify-center text-center rounded-sm mt-30 lg:max-w-md xl:max-w-lg lg:text-left md:p-6">
-          <h1 className="text-4xl font-bold leading-none md:text-5xl">
+      <div className="container paddings innerWidth flexCenter flex flex-col-reverse justify-center mx-auto px-20 sm:py-12 lg:py-24 md:flex-row lg:justify-between">
+        <div className="flex flex-col justify-center items-center text-center rounded-sm w-ful md:mt-30 lg:max-w-md xl:max-w-lg lg:text-left md:p-6 md:items-start">
+          <h1 className=" flex flex-col justify-center items-center font-bold leading-none text-4xl md:flex-row lg:text-5xl">
             Let's learn
-            <span className="text-primary">Together</span>
+            <span className="text-primary left-0 flex justify-start items-start">
+              Together
+            </span>
           </h1>
-          <p className="mt-6 mb-8 text-lg sm:mb-12">
+          <p className="flex text-start mt-6 mb-8 text-md md:text-lg lg:text-xl sm:mb-12">
             If you are pursuing a BSC or a diploma in Computer science and don't
             <br className="hidden md:inline lg:hidden" />
             know which profession is more suitable for you then this is the
             documentation that might help you.
           </p>
-          <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+          <div className="flex flex-col space-y-4 w-full sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
             <a
               rel="noopener noreferrer"
               href="#"
@@ -26,7 +28,7 @@ const Header = () => {
             </a>
           </div>
         </div>
-        <div className="flex items-center justify-center p-0 h-72 md:p-6 lg:mt-30 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+        <div className="flex items-center justify-center p-0 h-72 mt-10 md:p-6 lg:mt-30 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           <img
             src="https://cdn.dribbble.com/users/926537/screenshots/4502924/media/79e26abb3fb85b42f2722cf22da095dc.gif"
             alt=""
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
   };
 
   return (
-    <header className="absolute bg-white w-full text-gray-600 body-font overflow-hidden">
-      <div className="container w-full mx-auto flex justify-between flex-wrap py-5 px-5 flex-row items-center md:px-10">
+    <header className="absolute bg-white w-full text-gray-600 body-font ">
+      <div className="container w-full mx-auto flex justify-between flex-wrap py-5 px-5 flex-row items-center md:px-2 lg:px-5">
         <a className="flex title-font  font-medium justify-center items-center text-gray-900 ">
-          <span className="ml-3 text-2xl font-bold md:text-4xl">
+          <span className="ml-3 font-bold text-2xl lg:text-4xl">
             Let's <span className="text-primary">Help</span>
           </span>
         </a>
@@ -29,17 +29,17 @@ const Navbar = () => {
             className="flex flex-wrap items-center text-base justify-between h-menu md:ml-auto"
             style={getMenuStyle(menuOpened)}
           >
-            <div className="flex flex-col gap-4 md:gap- md:flex-row">
-              <a className="mr-5 p-2 font-semibold text-md hover:text-gray-900 active:text-gray-950 md:text-lg">
+            <div className="flex flex-col gap-4 md:gap-0 lg:gap-6 md:flex-row">
+              <a className="mr-5 p-2 font-semibold text-md hover:text-gray-900 active:text-gray-950 lg:text-lg">
                 Home
               </a>
-              <a className="mr-5 p-2 font-semibold text-md hover:text-gray-900 active:text-gray-950 md:text-lg">
+              <a className="mr-5 p-2 font-semibold text-md hover:text-gray-900 active:text-gray-950 lg:text-lg">
                 Resource Docs
               </a>
-              <a className="mr-5 p-2 font-semibold text-md hover:text-gray-900 active:text-gray-950 md:text-lg">
+              <a className="mr-5 p-2 font-semibold text-md hover:text-gray-900 active:text-gray-950 lg:text-lg">
                 Choose Career
               </a>
-              <a className="transition-all mr-5 border rounded p-2 font-semibold text-md flex justify-center items-center gap-1 hover:text-white hover:bg-orange-500 active:bg-orange-500/30 active:text-white/80 md:text-lg">
+              <a className="transition-all mr-5 border rounded p-2 font-semibold text-md flex justify-center items-center gap-1 hover:text-white hover:bg-orange-500 active:bg-orange-500/30 active:text-white/80 lg:text-lg">
                 Roadmaps <FaArrowRight />
               </a>
             </div>
