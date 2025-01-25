02ec46c2086ee7187d8de712cc5c40978a28dcc8
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-01-25 12:54:55 +0600

    added two new components header and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
 }
 
 export default App;
